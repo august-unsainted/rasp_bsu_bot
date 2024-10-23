b28a0ca8b0db094f404be2311114041789bb10d3
august-unsainted <mhidt_unsainted@mail.ru>
2024-10-23 10:20:31 +0800

    добавление дат, указание сегодняшнего дня

--- utils/time_functions.py
+++ utils/time_functions.py
@@ -37,10 +37,10 @@ def find_date(day: str) -> str:
         for i in range(7):
             weekday_date = start_of_week + timedelta(days=i)
             weekday = datetime.strftime(weekday_date, '%A (%e %B)').capitalize().lstrip()
-            if weekday[-2] in 'йь':
-                weekday = weekday[:-2] + 'я)'
-            else:
-                weekday = weekday[:-1] + 'а)'
+            # if weekday[-2] in 'йь':
+            #     weekday = weekday[:-2] + 'я)'
+            # else:
+            #     weekday = weekday[:-1] + 'а)'
             week_dates.append(weekday)
         return '\n'.join(week_dates)
     elif day == 'Завтра':
@@ -49,8 +49,8 @@ def find_date(day: str) -> str:
         rasp_day = datetime.today()
 
     date = datetime.strftime(rasp_day, '%A (%e %B)').capitalize().lstrip()
-    if date[-2] in 'йь':
-        date = date[:-2] + 'я)'
-    else:
-        date = date[:-1] + 'а)'
+    # if date[-2] in 'йь':
+    #     date = date[:-2] + 'я)'
+    # else:
+    #     date = date[:-1] + 'а)'
     return date
